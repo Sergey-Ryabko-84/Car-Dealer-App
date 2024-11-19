1e232893f19873362d64a86efc143adbfb55902d
serhii.ryabko <serhii.ryabko@gmail.com>
2024-11-19 15:23:20 +0200

    Initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main className="flex flex-col gap-8 items-center p-10">
+      <h1 className="text-3xl font-semibold">Car Dealer App</h1>
+    </main>
+  );
+}
